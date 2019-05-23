5fedc206dfab2a72dd622ef0e6c5aabd2b6eb642
jonathonkindle <jonathon.kindle@seattlecentral.edu>
2019-05-22 21:16:23 -0700

    added new template, and view for resource page

--- PythonClubProject/PythonClubApp/views.py
+++ PythonClubProject/PythonClubApp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Meeting, MeetingMinutes, ResourceType, Resource, Event
 
 # Create your views here.
 def index (request):
     return render(request, 'PythonClubApp/index.html')
+
+def getresources(request):
+    resource_list=Resource.objects.all()
+    return render(request, 'PythonClubApp/resources.html', {'resource_list' : resource_list})
--- TechReviewProject/TechReviewApp/views.py
+++ TechReviewProject/TechReviewApp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import ProductType, Product, Review
 
 # Create your views here.
-def index (request):
+def index(request):
     return render(request, 'TechReviewApp/index.html')
+
+def gettypes(request):
+    type_list=ProductType.objects.all()
+    return render(request, 'TechReviewApp/types.html', {'type_list' : type_list})
